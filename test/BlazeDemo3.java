e94a8060e2db806412ed60c90fde1097f8235d94
Navjot Gill <Navjot Gill@NavjotGill>
2019-05-16 12:10:01 -0400

    SnakeTest Done

---
+++ test/BlazeDemo3.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BlazeDemo3 {
+
+	final String URL = "http://www.BlazeDemo.com";
+	final String DRIVER_PATH = "C:\\chromedriver.exe";
+	WebDriver driver;
+	
+	@Before
+	public void setUp() throws Exception {
+		// Setup Selenium + Chrome
+		System.setProperty("webdriver.chrome.driver",DRIVER_PATH);
+		driver = new ChromeDriver();
+		
+		// Tell Selenium what page to test
+		driver.get(URL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// At end of test case, wait for a few seconds, then close the browser
+		Thread.sleep(2000);				// OPTION 1
+		driver.close();
+
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	
+	
+
+}
--- test/SnakeTest.java
+++ test/SnakeTest.java
@@ -1,3 +1,7 @@
+//Ramandeep  Kaur
+//C0736211
+
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -15,10 +19,36 @@ public class SnakeTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHealthy() {
+		//fail("Not yet implemented");
+		
+		//check is healthy funtion is working 
+		boolean healthy = takis.isHealthy();
+		assertEquals(true, healthy);
+		System.out.println("takis is healthy");
+		
+		boolean nothealthy = peter.isHealthy();
+		assertEquals(false, nothealthy);
+		System.out.println("peter is unhealthy");
+			
+		//cage length > snake length
+		boolean fitcage1 = peter.fitsInCage(12);
+		assertEquals(true,fitcage1);
+		System.out.println("peter fits in cage");
+		
+		//cage length < snake length
+		boolean fitcage2 = takis.fitsInCage(45);
+		assertEquals(false,fitcage2);
+		System.out.println("takis not fits in cage");
+		
+		//cage length = snake length
+				boolean fitcage3 = peter.fitsInCage(10);
+				assertEquals(true,fitcage1);
+				System.out.println("peter fits in cage");
+		
 	}
-
+	
+	
 
 	
 
